11f8fc16edb258de3e0d3e4d6d3253458f30d33c
HansaemOh513 <number513@hanyang.ac.kr>
2024-05-09 20:07:34 +0900

    update

--- dataloader.py
+++ dataloader.py
@@ -7,78 +7,88 @@ import matplotlib.pyplot as plt
 class loader:
     def __init__(self, master):
         self.master = master
-    def orb_function(self, new):
-        orb = cv2.ORB_create()
+    def orb_function(self, new, **kwargs):
+        orb = cv2.ORB_create(**kwargs)
         kp_master, desc_master = orb.detectAndCompute(self.master, None)
         kp_new, desc_new = orb.detectAndCompute(new, None)
         bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.match(desc_master, desc_new)
+        try:
+            matches = bf.match(desc_master, desc_new)
+        except cv2.error as e:
+            print("Error!!!", e)
+            return None, 0
         matches = sorted(matches, key=lambda x: x.distance)
-        good_matches=matches[0:50]
-        total_x = 0
-        total_y = 0
-        for match in good_matches:
-            new_idx = match.trainIdx
-            total_x += kp_new[new_idx].pt[0]
-            total_y += kp_new[new_idx].pt[1]
-        num_matches = len(good_matches)
-        center_x = total_x / num_matches
-        center_y = total_y / num_matches
-        image = new[int(center_y) - 150:int(center_y) + 150, int(center_x) - 150:int(center_x) + 150, :]
-        fig, ax = plt.subplots()
-        ax.imshow(new)
-        ax.scatter([center_x], [center_y], color='green', s=100)
-        ax.text(center_x + 10, center_y + 10, 'Center', color='white', fontsize=12)
-        plt.show()
+        if len(matches)==0:
+            print("Error!!! Image is not matched at all.")
+            return None, 0
+        else:
+            good_matches=matches[0:30]
+            total_x = 0
+            total_y = 0
+            for match in good_matches:
+                new_idx = match.trainIdx
+                total_x += kp_new[new_idx].pt[0]
+                total_y += kp_new[new_idx].pt[1]
+            num_matches = len(good_matches)
+            center_x = total_x / num_matches
+            center_y = total_y / num_matches
+            image = new[int(center_y) - 150:int(center_y) + 150, int(center_x) - 150:int(center_x) + 150, :]
+            return image, 1
 
-        return image
-
-    def data_loader(self, path, resize):
+    def data_loader(self, n, path, resize):
         '''
-        path 를 입력으로 받음, stop, N=1000은 옵션. step = True로 하고 N개의 데이터만 콜렉팅함.
-        이미지를 정규화해서 리턴.
+        Input : 
+        n      : counting loaded data variable
+        path   : data path
+        resize : [0] is an option for resizing. If [0] is set to true, the output image will be resized to ([1], [2]) (width, height).
+
+        Output : n and images
         '''
         images = []
         
-        n = 0
         for name in os.listdir(path):
             image_path = os.path.join(path, name)
             image = cv2.imread(image_path)
-            image = self.orb_function(image)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            if resize[1]:
-                image = cv2.resize(image, (resize[2], resize[3])) # width, height
-            images.append(image/255)
-            if n % 100 == 0:
-                print("Loading ...", n)
-            n += 1
+            image, switch = self.orb_function(image)
+            if switch==0:
+                pass
+            else:
+                if image.shape[0]==0 or image.shape[1]==0:
+                    print("Error!!! Image has zero size.")
+                    pass
+                else:
+                    if resize[0]:
+                        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                        image = cv2.resize(image, (resize[1], resize[2])) # width, height
+                images.append(image)
+                n += 1
         
         images_np = np.array(images)
-        
-        return images_np
+        return n, images_np
 
-    def MS_loader(self, MS_list, resize = [False, False, 0, 0]):
+    def MS_loader(self, MS_list, resize = [False, None, None]):
         '''
-        인풋으로 MS_list를 받음. 예시 :
-        MS_list =  [['3029C005AA', 'step_1'], ['3029C006AA', 'step_1'], ['3029C009AA', 'step_1'], ['3029C010AA', 'step_1'],
-                    ['3030C002AA', 'step_1'], ['3030C003AA', 'step_1'], ['3030C004AA', 'step_1'], ['3031C001AA', 'step_1'],
-                    ['3031C002AA', 'step_1'], ['3031C003AA', 'step_1']]
-        MS_list 안에 첫 번째 요소로 Mtype이 있으며 두 번째 요소로 step이 있음.
+        Input : MS_list and resize
+        Output : Whole data chosen by MS_list
         '''
-        def data_shape():
-            for Mtype, step in MS_list:
-                Mtype_path = os.path.join('../../images', Mtype) # '../../images' 주의할 것.
-                load_path = os.path.join(Mtype_path, step) # '../../images' 주의할 것.
-                Mtype_data = self.data_loader(load_path, resize)
-                break
-            _, height, width, channel =  Mtype_data.shape
-            return (0, height, width, channel)
-        data = np.empty(data_shape())
         for Mtype, step in MS_list:
-            
             Mtype_path = os.path.join('../../images', Mtype) # '../../images' 주의할 것.
             load_path = os.path.join(Mtype_path, step) # '../../images' 주의할 것.
-            Mtype_data = self.data_loader(load_path, resize)
+            name = os.listdir(load_path)[0]
+            image_path = os.path.join(load_path, name)
+            image = cv2.imread(image_path)
+            if resize[0]:
+                image = cv2.resize(image, (resize[1], resize[2]))
+            height, width, channel = image.shape
+            data = np.empty((0, height, width, channel))
+            break
+        n = 0 # Initialize #
+        for Mtype, step in MS_list:
+            
+            Mtype_path = os.path.join('../../images', Mtype) # '../../images' warnings!
+            load_path = os.path.join(Mtype_path, step) # '../../images' warnings!
+            n, Mtype_data = self.data_loader(n, load_path, resize)
             data = np.concatenate([data, Mtype_data], axis = 0)
+            print("Loaded data : ", n)
         return data
 
---
+++ ordering.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+import glob
+import cv2
+import sys
+import re
+destination_path = "./images"
+def move():
+    path_1 = "./data" # Top level folder containing the data
+    folder_1 = os.listdir(path_1) # Machine types
+    for label in folder_1:
+        path_2 = os.path.join(path_1, label) 
+
+        folder_2 = os.listdir(path_2) # Machine information
+        for printer in folder_2:
+            path_3 = os.path.join(path_2, printer)
+            folder_3 = os.listdir(path_3) # NG or OK information
+            for name in folder_3 :
+                path_4 = os.path.join(path_3, name) # Image path
+                if re.search("jpg", name):
+                    if re.search(pattern, name):
+                        print(destination_path, label, pattern_)
+                        
+                        pattern__ = re.sub(".jpg", "", pattern_)
+                        os.makedirs(os.path.join(destination_path, label, pattern__), exist_ok=True)
+                        shutil.copy(path_4, os.path.join(destination_path, label, pattern__, printer + name))
+
+for i in range(30):
+    pattern  = f'step_{i}.jpg'
+    pattern_ = f'step_{i}.jpg'
+    move()
+
+
